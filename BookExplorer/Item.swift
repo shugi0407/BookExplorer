c48ede9ddfe3cb99419195c926c63602ae753676
Shugyla <rafikovashugyla@gmail.com>
2026-02-12 15:45:40 +0500

    Initial Commit

---
+++ BookExplorer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BookExplorer
+//
+//  Created by Twilight on 12.02.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
